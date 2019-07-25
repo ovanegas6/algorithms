41a38c13a7bcd59677854282aeb02fb6abe4fc18
ovanegas6 <ovanegas6@gmail.com>
2019-07-25 14:13:22 -0700

    Add javascript selection sort

---
+++ selection_sort.js
@@ -0,0 +1,25 @@
+//selection sort helper function
+function findSmallest(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[j] < smallest) {
+      smallest = arr[j];// 0
+      smallestIndex = j;// 1
+    } 
+  }
+  return smallestIndex;
+}
+
+//selection sort
+function selectionSort(arr) {
+  let newArr = [];
+  let length = arr.length;
+  
+  for (let k = 0; k < length; k++) {
+    let smallest = findSmallest(arr);
+    newArr.push(arr.splice(smallest, 1));
+  }
+  return newArr;
+}
